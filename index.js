d7f156680370d5aabfd3aaac255cbcef9d45bf44
Tristin Sorrells <Tristinsorrells1@gmail.com>
2022-12-15 17:36:24 -0600

    Fix bug causing repeat letters to be ignored

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var winningWord = '';
 var currentRow = 1;
 var guess = '';
 var gamesPlayed = [];
+let words
+let winner
 
 // Query Selectors
 var inputs = document.querySelectorAll('input');
@@ -19,9 +21,29 @@ var stats = document.querySelector('#stats-section');
 var gameOverBox = document.querySelector('#game-over-section');
 var gameOverGuessCount = document.querySelector('#game-over-guesses-count');
 var gameOverGuessGrammar = document.querySelector('#game-over-guesses-plural');
+var endGameText = document.querySelector('.game-result-message');
+var gameCount = document.querySelector('#stats-total-games');
+var percentOfGamesWon = document.querySelector("#stats-percent-correct");
+var statsAverageGuesses = document.querySelector('#stats-average-guesses')
 
 // Event Listeners
-window.addEventListener('load', setGame);
+window.addEventListener('load', (event) => {
+  fetchPromise()
+})
+
+function fetchAPI() {
+		return fetch(`http://localhost:3001/api/v1/words`)
+			.then((response) => response.json())
+			.then((data) => {return data})
+			.catch((error) => console.log(`API Error!`));
+	};
+  
+  const fetchPromise = () => {
+    fetchAPI().then((response) => {
+      words = response
+      setGame()
+    })
+  }
 
 for (var i = 0; i < inputs.length; i++) {
   inputs[i].addEventListener('keyup', function() { moveToNextInput(event) });
@@ -65,12 +87,12 @@ function updateInputPermissions() {
 
 function moveToNextInput(e) {
   var key = e.keyCode || e.charCode;
-
-  if( key !== 8 && key !== 46 ) {
-    var indexOfNext = parseInt(e.target.id.split('-')[2]) + 1;
-    inputs[indexOfNext].focus();
+  if ( key !== 8 && key !== 46 ) {
+      var indexOfNext = parseInt(e.target.id.split('-')[2]) + 1;
+      inputs[indexOfNext].focus();
+    }
   }
-}
+
 
 function clickLetter(e) {
   var activeInput = null;
@@ -93,7 +115,11 @@ function submitGuess() {
     compareGuess();
     if (checkForWin()) {
       setTimeout(declareWinner, 1000);
-    } else {
+    } 
+    else if (!checkForWin() && currentRow >= 6) {
+      setTimeout(declareWinner, 1000);
+    }
+    else {
       changeRow();
     }
   } else {
@@ -114,46 +140,97 @@ function checkIsWord() {
 }
 
 function compareGuess() {
-  var guessLetters = guess.split('');
+  var guessLetters = guess.split('')
+  
+  guessLetters.forEach((letter) => {
+    let currentLetter = guessLetters.indexOf(letter)
+    let correctWord = winningWord
 
-  for (var i = 0; i < guessLetters.length; i++) {
+    if (winningWord.includes(letter)) {
+      storeLetters()
+   
 
-    if (winningWord.includes(guessLetters[i]) && winningWord.split('')[i] !== guessLetters[i]) {
-      updateBoxColor(i, 'wrong-location');
-      updateKeyColor(guessLetters[i], 'wrong-location-key');
-    } else if (winningWord.split('')[i] === guessLetters[i]) {
-      updateBoxColor(i, 'correct-location');
-      updateKeyColor(guessLetters[i], 'correct-location-key');
-    } else {
-      updateBoxColor(i, 'wrong');
-      updateKeyColor(guessLetters[i], 'wrong-key');
+      // if (guess.indexOf(letter) === correctWord.indexOf(letter)) {
+      //   updateKeyColor(letter, "correct-location-key");
+      //   updateBoxColor(letter, "correct-location");
+      // }
+      // else {
+			// 	updateKeyColor(letter, "wrong-location-key");
+			// 	updateBoxColor(letter, "wrong-location");
+			// }
     }
-  }
+    else if (!winningWord.includes(letter)) {
+      updateKeyColor(letter, "wrong-key");
+      updateBoxColor(letter, "wrong");
+    }
+	})
+}
+
+function storeLetters() {
+  let counter1 = 0
+  let counter2 = 0
+
+   let guesses = guess.split("").reduce((accum, l) => {
+			accum[counter1] = l 
+			counter1++;
+			return accum;
+		}, []);
+
+    let answers = winningWord.split('').reduce((accum, l) => {
+      accum[counter2] = l 
+      counter2++
+      return accum 
+  }, [])
+
+  guesses.forEach((letter) => {
+    answers.forEach((answer) => {
+      if (letter === answer) {
+        updateKeyColor(letter, "correct-location-key");
+        updateBoxColor(letter, "correct-location");
+      }
+      else {
+				updateKeyColor(letter, "wrong-location-key");
+				updateBoxColor(letter, "wrong-location");
+			}
+    })
+  })
 
 }
 
-function updateBoxColor(letterLocation, className) {
+
+
+function updateBoxColor(letter, className) {
   var row = [];
+  let counter = 0
 
-  for (var i = 0; i < inputs.length; i++) {
-    if(inputs[i].id.includes(`-${currentRow}-`)) {
-      row.push(inputs[i]);
+  inputs.forEach((input) => {
+    let rowNumber = input.id.split('-')
+    if (parseInt(rowNumber[1]) === currentRow) {
+      row.push(input)  
     }
-  }
+  })
+  
+  let letterMatches = guess.split('').reduce((accum, l) => {
+      accum[counter] = l + "-" + counter
+      counter++
+      return accum 
+  }, {})
 
-  row[letterLocation].classList.add(className);
+  Object.values(letterMatches).forEach((l) => {
+    l = l.split('-')
+    let letterLocation = l[1];
+    if (l[0] === letter) {
+      row[letterLocation].classList.add(className);
+    }
+  })
 }
 
 function updateKeyColor(letter, className) {
-  var keyLetter = null;
-
-  for (var i = 0; i < keyLetters.length; i++) {
-    if (keyLetters[i].innerText === letter) {
-      keyLetter = keyLetters[i];
+  keyLetters.forEach((key) => {
+    if (key.innerText === letter) {
+      key.classList.add(className);
     }
-  }
-
-  keyLetter.classList.add(className);
+  })
 }
 
 function checkForWin() {
@@ -166,23 +243,28 @@ function changeRow() {
 }
 
 function declareWinner() {
+  if (checkForWin()) {
+    endGameText.innerText = `You Win! The correct word was ${winningWord}`
+  } 
+  else {
+    endGameText.innerText = `You Lose! The correct word was ${winningWord}`;
+  }
   recordGameStats();
   changeGameOverText();
   viewGameOverMessage();
   setTimeout(startNewGame, 4000);
 }
 
+
 function recordGameStats() {
-  gamesPlayed.push({ solved: true, guesses: currentRow });
+  if (checkForWin()) {
+    return gamesPlayed.push({ solved: true, guesses: currentRow });
+	} 
+  return gamesPlayed.push({ solved: false, guesses: currentRow });
 }
 
 function changeGameOverText() {
   gameOverGuessCount.innerText = currentRow;
-  if (currentRow < 2) {
-    gameOverGuessGrammar.classList.add('collapsed');
-  } else {
-    gameOverGuessGrammar.classList.remove('collapsed');
-  }
 }
 
 function startNewGame() {
@@ -237,6 +319,14 @@ function viewStats() {
   viewGameButton.classList.remove('active');
   viewRulesButton.classList.remove('active');
   viewStatsButton.classList.add('active');
+  gameCount.innerText = gamesPlayed.length
+  let numberOfWins = gamesPlayed.filter(game => game.solved)
+  percentOfGamesWon.innerText = ((numberOfWins.length / gamesPlayed.length) * 100).toFixed(2)
+   let averageGuessCount = numberOfWins.reduce((accum, game) => {
+    accum += game.guesses
+    return accum
+  }, 0) / numberOfWins.length
+  statsAverageGuesses.innerText = averageGuessCount.toFixed(0)
 }
 
 function viewGameOverMessage() {
